e560be71a4e5324a6f3aa2c87b8afa1d21b15d67
meme121313 <83842783+meme121313@users.noreply.github.com>
2021-06-04 14:14:24 +0300

    2nd commit

---
+++ app/src/main/java/com/example/myapplication/User_registration_step3.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_registration_step3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_registration_step3);
+    }
+}
